e1b9b1405f537cc81765bacfac0a344e3c9aadab
Joey <joey.mckenzie27@gmail.com>
2023-12-21 14:49:43 -0800

    chore: add vscode settings

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
-use std::sync::{Arc, Mutex};
-
 use anyhow::Context;
-use axum::{
-    extract::State,
-    http::StatusCode,
-    response::{Html, IntoResponse, Response},
-    routing::{get, post},
-    Form, Router,
-};
-use serde::Deserialize;
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv()
+        .context("failed to load environment, make sure a valid .env file is available")?;
+
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "with_axum_htmx_askama=debug".into()),
+                .unwrap_or_else(|_| "salesforce_api=debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
 
     info!("initializing router...");
 
+    Ok(())
 }
